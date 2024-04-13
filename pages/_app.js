59a52737c04543ab02d25f096c54f3ec1d443b9a
Techomoro <techomoroblog@gmail.com>
2024-04-14 02:11:47 +0530

    google analytics added

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,15 @@
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "../styles/globals.css";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <GoogleTagManager gtmId="G-1J7V48B98E" />
+    </>
+  );
 }
 
 export default MyApp;
